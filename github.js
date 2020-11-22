4dfe22bff413f443c8dd11793961dbd2f42c3d15
Monika171 <monikarabha171@gmail.com>
2020-11-23 07:45:11 +0900

    Github-hunter: A JS App

---
+++ github.js
@@ -0,0 +1,22 @@
+class Github {
+  constructor() {
+    this.client_id = 'f0894795f6bb860cde6a';
+    this.client_secret = '0715fcdf45bd32b41ce9dd5736a7d0de8e4b91d2';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos
+    }
+  }
+}
